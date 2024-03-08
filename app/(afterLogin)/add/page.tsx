95cead2bdcccae19afcf9f54ba327f03d8ab8c9d
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2024-03-08 15:57:24 +0900

    feat : 다른 페이지 파일만 생성.

---
+++ app/(afterLogin)/add/page.tsx
@@ -0,0 +1,5 @@
+const AddMisigdoPage = () => {
+  return <div>Misigdo Add Page</div>;
+};
+
+export default AddMisigdoPage;
---
+++ app/(afterLogin)/group/page.tsx
@@ -0,0 +1,7 @@
+const GroupListPage = () => {
+  return (
+    <div>Group List Page</div>
+  )
+}
+
+export default GroupListPage;
