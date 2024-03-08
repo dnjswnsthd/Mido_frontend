ab7a99b097e625e5479da793548813d39d46f840
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2024-03-08 16:25:47 +0900

    fix : AfterLoginLayout props 수정  - 사용하지 않는 modal 삭제(추후 추가)

--- app/(afterLogin)/layout.tsx
+++ app/(afterLogin)/layout.tsx
@@ -1,15 +1,14 @@
 import { ReactNode } from "react";
 import style from "./page.module.scss";
 import NavigationBar from "./_component/NavigationBar";
-type AfterLoginLoyoutProps = { children: ReactNode, modal: ReactNode };
-const AfterLoginLoyout = ({ children, modal }: AfterLoginLoyoutProps) => {
+type AfterLoginLoyoutProps = { children: ReactNode };
+const AfterLoginLoyout = ({ children }: AfterLoginLoyoutProps) => {
   return (
     <div className={style.layout_container}>
       <div className={style.content_container}>
         {children}
         <NavigationBar />
       </div>
-      {modal}
     </div>
   );
 };
