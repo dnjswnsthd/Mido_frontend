15dadd57525a27f3d758f2a9fe516ab715c96a7f
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2024-03-10 12:00:10 +0900

    fix : video tag 속성추가, type 추가

--- app/(beforeLogin)/_component/Main.tsx
+++ app/(beforeLogin)/_component/Main.tsx
@@ -5,14 +5,22 @@ const Main = () => {
   return (
     <div className={style.main_container}>
       <div className={style.logo_image}>
-        <video src={"/mido.mp4"} autoPlay muted loop />
+        <video autoPlay muted loop playsInline>
+          <source src={"/mido.mp4"} type="video/mp4"></source>
+        </video>
       </div>
       <div className={style.login_container}>
         <h3 className={style.start_message}>3초만에 시작하기</h3>
         <nav className={style.nav_container}>
-          <Link href="/home" className={style.nav_item}>K</Link>
-          <Link href="/home" className={style.nav_item}>N</Link>
-          <Link href="/home" className={style.nav_item}>G</Link>
+          <Link href="/home" className={style.nav_item}>
+            K
+          </Link>
+          <Link href="/home" className={style.nav_item}>
+            N
+          </Link>
+          <Link href="/home" className={style.nav_item}>
+            G
+          </Link>
         </nav>
       </div>
     </div>
