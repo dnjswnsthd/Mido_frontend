581f4702ffc6db336f1a2e4d8eb61092e81617b6
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2024-03-19 16:54:08 +0900

    feat : Group Item 디자인 수정

--- app/(afterLogin)/group/_component/GroupList.tsx
+++ app/(afterLogin)/group/_component/GroupList.tsx
@@ -1,11 +1,46 @@
+import { Pioneer } from "../../home/page";
 import GroupListItem from "./GroupListItem";
 import style from "./groupList.module.scss";
+
+
+export interface GroupInfo {
+  "group_id": number;
+  "group_name": string;
+  "pioneer_list": Pioneer[]
+}
+
+const sampleGroupInfo = [{
+  "group_id": 0,
+  "group_name": "구디 대한족발 팟",
+  "pioneer_list": [
+    { "pioneer_id": "user00", "pioneer_nickname": "SONG", "pioneer_image_url": "" },
+    { "pioneer_id": "user01", "pioneer_nickname": "JuneHyung", "pioneer_image_url": "" },
+    { "pioneer_id": "user02", "pioneer_nickname": "Soon", "pioneer_image_url": "" }
+  ],
+},{
+  "group_id": 1,
+  "group_name": "미식도 내이름은 비룡",
+  "pioneer_list": [
+    { "pioneer_id": "user00", "pioneer_nickname": "SONG", "pioneer_image_url": "" },
+    { "pioneer_id": "user01", "pioneer_nickname": "JuneHyung", "pioneer_image_url": "" },
+    { "pioneer_id": "user02", "pioneer_nickname": "Soon", "pioneer_image_url": "" }
+  ],
+},{
+  "group_id": 2,
+  "group_name": "구디 대한족발 팟2",
+  "pioneer_list": [
+    { "pioneer_id": "user00", "pioneer_nickname": "SONG", "pioneer_image_url": "" },
+    { "pioneer_id": "user01", "pioneer_nickname": "JuneHyung", "pioneer_image_url": "" },
+    { "pioneer_id": "user02", "pioneer_nickname": "Soon", "pioneer_image_url": "" }
+  ],
+}]
+
 const GroupList = () => {
   return (
     <ul className={style.group_list}>
-      <GroupListItem />
-      <GroupListItem />
-      <GroupListItem />
+      {sampleGroupInfo.map((info)=>{
+        return <GroupListItem key={info.group_id} info={info}/>
+      })}
     </ul>
   )
 }
--- app/(afterLogin)/group/_component/GroupListItem.tsx
+++ app/(afterLogin)/group/_component/GroupListItem.tsx
@@ -1,30 +1,42 @@
 import MdiIcon from "../../_component/MdiIcon";
+import Profile from "../../_component/Profile";
+import { GroupInfo } from "./GroupList";
 import style from "./groupList.module.scss";
 
-const GroupListItem = () => {
+interface GroupListItemProps {info: GroupInfo}
+const GroupListItem = ({info}: GroupListItemProps) => {
   return (
     <li className={style.group_list_item}>
       <div className={style.top_area}>
         <h3 className={style.group_title}>
-          미식도 내 이름은 비룡
+          {info.group_name}
           <div className={style.pioneer_cnt}>
-            <span>h</span>
-            <span>3</span>
+            <MdiIcon path="mdiAccount" size={16}/>
+            <span>{info.pioneer_list.length}</span>
           </div>
         </h3>
         <div className={style.button_wrap}>
           <button>
-            <MdiIcon path={'mdiPencil'} size={20} color={'#fff'}/>
+            <MdiIcon path={"mdiPencil"} size={20} color={"#fff"} />
           </button>
           <button>
-            <MdiIcon path={'mdiDelete'} size={20} color={'#fff'}/>
+            <MdiIcon path={"mdiDelete"} size={20} color={"#fff"} />
           </button>
         </div>
       </div>
       <ul className={style.user_list}>
-        <li>song</li>
-        <li>JuneHyung</li>
-        <li>순</li>
+        {
+          info.pioneer_list.map((pioneer)=>{
+            return (
+              <li key={pioneer.pioneer_id} className={style.user_info}>
+                
+                <Profile />
+                
+                <p className={`${style.user_name}`}>{pioneer.pioneer_nickname}</p>
+              </li>
+            )
+          })
+        }
       </ul>
     </li>
   );
